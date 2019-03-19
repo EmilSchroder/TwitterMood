c72e8c486c70f79d0770ba65e5f170f45e8ad433
EmilSchroder <eschro66@gmail.com>
2019-03-19 17:39:29 +1300

    fiddling parameters

--- textgrab.js
+++ textgrab.js
@@ -2,3 +2,18 @@ const config = require('./config')
 const Twit = require('twit')
 
 const T = new Twit(config)
+
+let params = {
+    q: 'happy',
+    count: 1
+}
+
+T.get('search/tweets', params, haveData);
+
+function haveData(err, data, response){
+    if(err){
+        console.log(err)
+    } else {
+        console.log(data)
+    }
+}
